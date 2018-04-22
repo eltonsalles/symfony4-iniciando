563a58d2e57f7c5af5f4aeaf34fe54495d2567ae
Elton Rodrigues <eltonrms.leite@hotmail.com>
2018-04-22 11:43:27 -0300

    criado o método create e ajustado a rota da index para manter o padrão de nomes

--- src/Controller/ProdutoController.php
+++ src/Controller/ProdutoController.php
@@ -3,13 +3,15 @@
 namespace App\Controller;
 
 use App\Entity\Produto;
+use App\Form\ProdutoType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class ProdutoController extends Controller
 {
     /**
-     * @Route("/produto", name="produto")
+     * @Route("/produto", name="listar_produto")
      */
     public function index()
     {
@@ -18,6 +20,34 @@ class ProdutoController extends Controller
 
         return $this->render("produto/index.html.twig", [
             'produtos' => $produtos
+        ]);
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @Route("/produto/cadastrar", name="cadastrar_nome")
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function create(Request $request)
+    {
+        $produto = new Produto();
+        $form = $this->createForm(ProdutoType::class, $produto);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($produto);
+            $em->flush();
+
+            $this->get('session')->getFlashBag()->set('success', 'Produto salvo com sucesso!');
+
+            return $this->redirectToRoute('listar_produto');
+        }
+
+        return $this->render("produto/create.html.twig", [
+            'form' => $form->createView()
         ]);
     }
 }
