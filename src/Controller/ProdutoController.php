581638be6dabcbc49dd7b405dc9d4c02a2ec4e0c
Elton Rodrigues <eltonrms.leite@hotmail.com>
2018-04-22 12:15:07 -0300

    criado a action update

--- src/Controller/ProdutoController.php
+++ src/Controller/ProdutoController.php
@@ -26,7 +26,7 @@ class ProdutoController extends Controller
     /**
      * @param Request $request
      *
-     * @Route("/produto/cadastrar", name="cadastrar_nome")
+     * @Route("/produto/cadastrar", name="cadastrar_produto")
      * @return \Symfony\Component\HttpFoundation\Response
      */
     public function create(Request $request)
@@ -47,6 +47,36 @@ class ProdutoController extends Controller
         }
 
         return $this->render("produto/create.html.twig", [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @param Request $request
+     * @param $id
+     * @return \Symfony\Component\HttpFoundation\Response
+     *
+     * @Route("/produto/editar/{id}", name="editar_produto")
+     */
+    public function update(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $produto = $em->getRepository(Produto::class)->find($id);
+
+        $form = $this->createForm(ProdutoType::class, $produto);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($produto);
+            $em->flush();
+
+            $this->get('session')->getFlashBag()->set('success', 'Produto ' . $produto->getNome() . ' alterado com sucesso!');
+
+            return $this->redirectToRoute('listar_produto');
+        }
+
+        return $this->render("produto/update.html.twig", [
+            'produto' => $produto,
             'form' => $form->createView()
         ]);
     }
